dc81cfa782ca8e489a3dca520372062620af8174
Shreya Mishra <shreyamishra414@gmail.com>
2022-06-17 18:58:11 +0530

    adding gui

---
+++ gui.py
@@ -0,0 +1,300 @@
+
+from tkinter import *
+from tkinter import messagebox as mb
+
+from PIL import Image,ImageTk
+
+from guiclass import *
+
+from tkinter import filedialog as fd
+
+font="Times New Roman"
+
+def setMode(mode):
+    global color
+    if mode.upper()== "DARK":
+        color=DarkMode()
+    elif mode.upper()=="LIGHT":
+        color=LightMode()
+    
+    #print(color.getColors())
+    
+    
+def changeMode(*args):
+    for i in args:
+        if type(i)==Label:
+            i.config(fg=color.labelfg,
+            bg=color.labelbg,)
+        elif type(i)==Button:
+            i.config(fg=color.labelfg,bg=color.buttons)
+        elif type(i)==Entry:
+            i.config(fg=color.labelfg,bg=color.entry)
+        elif type(i)==Menu:
+            i.config(bg=color.labelbg,fg=color.labelfg)
+        elif type(i)==Frame:
+            i.config(bg=color.labelbg)
+        elif type(i)==Text:
+            i.config(fg=color.labelfg, bg=color.entry,insertbackground=color.labelfg)
+        
+
+setMode("LIGHT")
+
+def hide(*args):
+    for i in args:
+        i.pack_forget()
+    
+
+def main():
+    r=tkObject("Encryption","1300x600",(500,500))
+    root=r.getObject()
+    
+    
+
+    r.setBg(color)
+    
+
+    def dark():
+        setMode("DARK")
+        r.setBg(color)
+        changeMode(
+            mybtn,
+            header,
+            menubar,
+            filemenu,
+            mode,
+            mybtn3,
+            home_btn,
+            Open_btn,
+            textarea_btn,
+            encrypt_btn,
+            decrypt_btn,
+            frame,
+            text,
+            frame2,
+
+            )
+        mybtn.config(command=light)
+
+        
+
+    def light():
+        setMode("LIGHT")
+        r.setBg(color)
+        changeMode(
+            mybtn,
+            header,
+            menubar,
+            filemenu,
+            mode,
+            New_btn,
+            mybtn3,
+            home_btn,
+            textarea_btn,
+            encrypt_btn,
+            decrypt_btn,
+            frame,
+            text,
+            frame2,
+
+            )
+        mybtn.config(command=dark)
+
+    def activateText():
+        frame2.pack(side=TOP)
+
+        
+    frame=Frame(bg=color.labelbg)
+    frame2=Frame(root,bg=color.labelbg,width=1400,height=600)
+    frame.pack(side=TOP)
+    
+    myimg= PhotoImage(file="icons8-data-encryption-100.png")
+    p= myimg.subsample(3,3)
+
+    header=Label(
+        frame,
+        text="ENCRYPTION SYSTEM",
+        font=(font,20,"bold"),
+        padx=10,
+        pady=3,
+        bg=color.labelbg,
+        fg= color.labelfg,
+        image=p,
+        compound= LEFT,
+        relief=RAISED
+    )
+    header.pack(side=LEFT,fill=X)
+
+
+    myimg5= PhotoImage(file="icons8-close-100.png")
+    p5= myimg5.subsample(3,3)
+    mybtn3=Button(
+        frame,
+        command=lambda:root.destroy(),
+        bg=color.buttons,
+        fg=color.labelfg,
+        font= (font,12,"bold"),
+        padx=5,pady=2,
+        image=p5,
+        compound=LEFT,
+        )
+    mybtn3.pack(side=RIGHT)
+    CreateToolTip(mybtn3,"close window")
+
+
+         
+        
+    myimg2= PhotoImage(file="icons8-invert-colors-100.png")
+    p2= myimg2.subsample(3,3)
+    mybtn=Button(
+        frame,
+        command=lambda:light(),
+        bg=color.buttons,
+        fg=color.labelfg,
+        font= (font,12,"bold"),
+        padx=5,pady=2,
+        image=p2,
+        compound=LEFT,
+        
+        
+        )
+    mybtn.pack(side=RIGHT)
+    CreateToolTip(mybtn,"change mode")
+
+    myimg14= PhotoImage(file="icons8-data-encryption-100.png")
+    p14= myimg14.subsample(3,3)
+    decrypt_btn=Button(
+        frame,
+        command=lambda:None,
+        bg=color.buttons,
+        fg=color.labelfg,
+        font= (font,12,"bold"),
+        padx=5,pady=2,
+        image=p14,
+        compound=LEFT,
+        )
+    decrypt_btn.pack(side=RIGHT)
+    CreateToolTip(decrypt_btn,"Decrypt")
+
+    myimg15= PhotoImage(file="icons8-data-protection-100.png")
+    p15= myimg15.subsample(3,3)
+    encrypt_btn=Button(
+        frame,
+        command=lambda:None,
+        bg=color.buttons,
+        fg=color.labelfg,
+        font= (font,12,"bold"),
+        padx=5,pady=2,
+        image=p15,
+        compound=LEFT,
+        )
+    encrypt_btn.pack(side=RIGHT)
+    CreateToolTip(encrypt_btn,"Encrypt")
+   
+
+    
+    myimg17= PhotoImage(file="icons8-align-text-left-100.png")
+    p17= myimg17.subsample(3,3)
+    textarea_btn=Button(
+        frame,
+        command=lambda:activateText(),
+        bg=color.buttons,
+        fg=color.labelfg,
+        font= (font,12,"bold"),
+        padx=5,pady=2,
+        image=p17,
+        compound=LEFT,
+        )
+    textarea_btn.pack(side=RIGHT)
+    CreateToolTip(textarea_btn,"Text Area")
+
+
+    myimg11= PhotoImage(file="icons8-open-view-100.png")
+    p11= myimg11.subsample(3,3)
+    Open_btn=Button(
+        frame,
+        command=lambda:None,
+        bg=color.buttons,
+        fg=color.labelfg,
+        font= (font,12,"bold"),
+        padx=5,pady=2,
+        image=p11,
+        compound=LEFT,
+        )
+    Open_btn.pack(side=RIGHT)
+    CreateToolTip(Open_btn,"Open File")
+    myimg8= PhotoImage(file="icons8-home-100.png")
+    p8= myimg8.subsample(3,3)
+    home_btn=Button(
+        frame,
+        command=lambda:[root.destroy(),main()],
+        bg=color.buttons,
+        fg=color.labelfg,
+        font= (font,12,"bold"),
+        padx=5,pady=2,
+        image=p8,
+        compound=LEFT,
+        )
+    home_btn.pack(side=RIGHT)
+    CreateToolTip(home_btn,"Home")
+    
+    
+    global text
+    text=Text (
+        frame2,
+        width=500,
+        fg=color.labelfg,
+        bg=color.entry,
+        cursor="arrow",
+        insertbackground=color.labelfg,
+        font=(font,12,"normal"),
+        
+        
+    )
+    
+    s=Scrollbar(frame2)
+    s.pack(side=RIGHT,fill=Y)
+    text.config(yscrollcommand = s.set)
+    text.pack(side=TOP,fill=BOTH,expand=TRUE )
+    # frame2.pack(side=TOP)
+
+
+    menubar=Menu(root)
+    filemenu = Menu(menubar, tearoff=0,bg=color.labelbg,fg= color.labelfg,relief=RAISED,font=(font,12,"normal"))
+    menubar.add_cascade(label="File", menu=filemenu)
+    filemenu.add_command(label="Encrypt File",command=None)
+    filemenu.add_command(label="Decrypt File",command=None)
+
+    mode=Menu(menubar,tearoff=0,bg=color.labelbg,fg= color.labelfg,relief=RAISED,font=(font,12,"normal"))
+    menubar.add_cascade(label="Change Mode",menu=mode)
+    mode.add_command(label="Light Mode",command =light)
+    mode.add_command(label="Dark Mode",command =dark)
+
+    help=Menu(menubar,tearoff=0,bg=color.labelbg,fg= color.labelfg,relief=RAISED,font=(font,12,"normal"))
+    menubar.add_cascade(label="Help", menu=help)
+    help.add_command(label="Help",command=None)
+    help.add_command(label="How it works?",command=None)
+    help.add_command(label="FAQs",command=None)
+    
+    root.config(menu=menubar)
+
+
+    r.getMainloop()
+
+
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ guiclass.py
@@ -0,0 +1,216 @@
+
+from tkinter import *
+
+class Colors:
+    
+    def __init__(self,mode):
+        
+        #defining all colors here
+        if mode.lower()=='dark':
+            self.mode= "DARK"
+        elif mode.lower()=='light':
+            self.mode= "LIGHT"
+        else:
+            self.mode="DEFAULT"
+
+    
+    def set_colors(self,header,buttons,background,labelbg,labelfg,entry):
+        self.header=header
+        self.buttons=buttons
+        self.background=background
+        self.labelbg=labelbg
+        self.labelfg=labelfg
+        self.entry=entry
+
+    def getMode(self):
+        return self.mode
+
+    def setMode(self,mode):
+        self.__init__(self,self.object,mode)
+
+    def changeMode(self,mode):
+        if mode.upper()=="DARK":
+            return DarkMode(self)
+        elif mode.upper()=="LIGHT":
+            return LightMode(self)
+        else :
+            return DefaultMode(self)
+    
+    def getBg(self):
+        return self.background
+    
+    
+    
+class DarkMode(Colors):
+    def __init__(self,header="#0a0a0f",buttons="#33334d",background="#3d3d5c",labelbg="#3d3d5c",labelfg="white",entry="#0a0a0f"):
+        Colors.__init__(self,"DARK")
+        
+        self.header=header
+        self.buttons=buttons
+        self.background=background
+        self.labelbg=labelbg
+        self.labelfg=labelfg
+        self.entry=entry
+        self.set_colors()
+    
+    def getColors(self):
+        return self.header, self.buttons,self.background,self.labelfg,self.labelbg,self.entry
+    
+    def set_colors(self, header="#0a0a0f",buttons="#33334d",background="#3d3d5c",labelbg="#3d3d5c",labelfg="white",entry="#0a0a0f"):
+        return super().set_colors(header, buttons, background, labelbg, labelfg, entry)
+    
+class LightMode(Colors):
+    def __init__(self,header="#d9d9d9",buttons="#0099ff",background="#66ccff",labelbg="#66ccff",labelfg="black",entry="white"):
+        Colors.__init__(self,"LIGHT")
+        
+        self.header=header
+        self.buttons=buttons
+        self.background=background
+        self.labelbg=labelbg
+        self.labelfg=labelfg
+        self.entry=entry
+
+    def getColors(self):
+        return self.header, self.buttons,self.background,self.labelfg,self.labelbg,self.entry
+    
+    def set_colors(self, header="#d9d9d9", buttons="#0099ff", background="#66ccff", labelbg="#66ccff", labelfg="black", entry="white"):
+        return super().set_colors(header, buttons, background, labelbg, labelfg, entry)
+
+class DefaultMode(Colors):
+    def __init__(self,header,buttons,background,labelbg,labelfg,entry):
+        Colors.__init__(self,"DEFAULT")
+        
+        self.buttons=buttons
+        self.background=background
+        self.labelbg=labelbg
+        self.labelfg=labelfg
+        self.entry=entry
+        self.set_colors()
+
+    def getColors(self):
+        return self.header, self.buttons,self.background,self.labelfg,self.labelbg,self.entry
+
+    def set_colors(self, header, buttons, background, labelbg, labelfg, entry):
+        return super().set_colors(header, buttons, background, labelbg, labelfg, entry)
+
+"""
+class Mode(DarkMode or LightMode or DefaultMode):
+    def __init__(self,mode):
+        Colors.__init__(self,mode)
+        if self.getMode()=="DARK":
+            DarkMode.__init__(self)
+        elif self.getMode()=="LIGHT":
+            LightMode.__init__(self)
+        else:
+            DefaultMode.__init__(self)
+"""
+
+
+
+class tkObject:
+    def __init__(self,title,geometry,min):
+        self.title= title
+        #self.bg=bg
+        self.geometry = geometry
+        self.minsize= min
+        self.object=Tk()
+        self.__createWindow()
+        
+
+    def __createWindow(self):
+        self.object.geometry(self.geometry)
+        self.object.title(self.title)
+        self.object.minsize(self.minsize[0],self.minsize[1])
+        # self.object.iconbitmap("icons8-data-encryption-100.png")
+        #self.object.config(bg=self.bg)
+
+    def setBg(self,colorObject:Colors):
+        self.object.config(bg=colorObject.getBg())
+
+    def setObject(self,title,bg,geometry,min):
+        self.__init__(title,bg,geometry,min)
+
+    def getObject(self):
+        return self.object
+
+    def getMainloop(self):
+        self.object.mainloop()
+          
+import re
+
+class HoverInfo(Menu):
+    def __init__(self, parent, text, command=None):
+        self._com = command
+        Menu.__init__(self,parent, tearoff=0)
+        if not isinstance(text, str):
+            raise TypeError('Trying to initialise a Hover Menu with a non string type: ' + text.__class__.__name__)
+        toktext=re.split('\n', text)
+        for t in toktext:
+            self.add_command(label = t)
+            self._displayed=False
+            self.master.bind("<Enter>",self.Display )
+            self.master.bind("<Leave>",self.Remove )
+    
+    def __del__(self):
+        self.master.unbind("<Enter>")
+        self.master.unbind("<Leave>")
+ 
+    def Display(self,event):
+        if not self._displayed:
+            self._displayed=True
+            self.post(event.x_root, event.y_root)
+        if self._com != None:
+            self.master.unbind_all("<Return>")
+            self.master.bind_all("<Return>", self.Click)
+ 
+    def Remove(self, event):
+        if self._displayed:
+            self._displayed=False
+            self.unpost()
+        if self._com != None:
+            self.unbind_all("<Return>")
+        
+    def Click(self, event):
+        self._com()
+
+class ToolTip(object):
+
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 57
+        y = y + cy + self.widget.winfo_rooty() +27
+        self.tipwindow = tw = Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        label = Label(tw, text=self.text, justify=LEFT,
+                      background="#ffffe0", relief=SOLID, borderwidth=1,
+                      font=("Times New Roman", "10", "normal"))
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+
+def CreateToolTip(widget, text):
+    toolTip = ToolTip(widget)
+    def enter(event):
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+
+
+
